bbffa79143e4265d69ea6cf2fc99505915c734fe
XxidroxX <nicoales@live.it>
2013-04-24 16:50:34 +0200

    Add spanish language

---
+++ application/language/spanish/home_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+//Language string for homepage
+$lang['login']              = '';
+$lang['username']           = '';
+$lang['password']           = '';
+$lang['remember']           = '';
+
+$lang['title']              = '';
+$lang['slogan']             = '';
+$lang['log_description']    = '';
+
+$lang['register']           = '';
+$lang['reg_description']    = '';
+$lang['email']              = '';
+$lang['reg_button']         = '';
